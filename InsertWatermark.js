5dd898a91d3ced0add318ad5ec3debd1c378b687
ovchinnikova-natalya <71496518+ovchinnikova-natalya@users.noreply.github.com>
2020-09-25 16:43:29 +0300

    Delete InsertWatermark.js

--- InsertWatermark.js
+++
@@ -1,7 +0,0 @@
-builder.CreateFile("docx");
-var Document = Api.GetDocument();
-var Paragraph = Document.GetElement(0);
-Paragraph.AddText("This is just a sample text.");
-Document.InsertWatermark("Watermark");
-builder.SaveFile("docx", "InsertWatermark.docx");
-builder.CloseFile();
