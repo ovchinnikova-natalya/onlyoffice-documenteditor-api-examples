a1236ea21c1f42bd910838b5e0107de9be5c1679
ovchinnikova-natalya <71496518+ovchinnikova-natalya@users.noreply.github.com>
2020-09-25 16:41:31 +0300

    Delete CreateBullet.js

--- CreateBullet.js
+++
@@ -1,8 +0,0 @@
-builder.CreateFile("docx");
-var Document = Api.GetDocument();
-var Paragraph = Document.GetElement(0);
-var Bullet = Api.CreateBullet("-");
-Paragraph.SetBullet(Bullet);
-Paragraph.AddText("This is an example of the bulleted paragraph.");
-builder.SaveFile("docx", "CreateBullet.docx");
-builder.CloseFile();
