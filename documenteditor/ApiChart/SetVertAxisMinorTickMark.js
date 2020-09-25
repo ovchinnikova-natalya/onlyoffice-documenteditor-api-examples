eb6d87714b4a4cf90248d407457666421788d44d
ovchinnikova-natalya <71496518+ovchinnikova-natalya@users.noreply.github.com>
2020-09-25 16:35:53 +0300

    Add files via upload

---
+++ documenteditor/ApiChart/SetVertAxisMinorTickMark.js
@@ -0,0 +1,14 @@
+builder.CreateFile("docx");
+var Document = Api.GetDocument();
+var Paragraph = Document.GetElement(0);
+var Drawing = Api.CreateChart("bar3D", [
+  [200, 240, 280],
+  [250, 260, 280]
+], ["Projected Revenue", "Estimated Costs"], [2014, 2015, 2016], 4051300, 2347595, 24);
+Drawing.SetVerAxisTitle("USD In Hundred Thousands", 10);
+Drawing.SetHorAxisTitle("Year", 11);
+Drawing.SetVertAxisMinorTickMark("out");
+Drawing.SetTitle("Financial Overview", 13);
+Paragraph.AddDrawing(Drawing);
+builder.SaveFile("docx", "SetVertAxisMinorTickMark.docx");
+builder.CloseFile();
