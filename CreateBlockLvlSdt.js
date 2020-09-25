f14b0a9d87615d438862927cd10c1779739de8e8
ovchinnikova-natalya <71496518+ovchinnikova-natalya@users.noreply.github.com>
2020-09-25 16:41:19 +0300

    Delete CreateBlockLvlSdt.js

--- CreateBlockLvlSdt.js
+++
@@ -1,9 +0,0 @@
-builder.CreateFile("docx");
-var Document = Api.GetDocument();
-var BlockLvlSdt = Api.CreateBlockLvlSdt();
-var Paragraph = Api.CreateParagraph();
-Paragraph.AddText("This is a block text content control.");
-BlockLvlSdt.AddElement(Paragraph, 0);
-Document.AddElement(BlockLvlSdt);
-builder.SaveFile("docx", "CreateBlockLvlSdt.docx");
-builder.CloseFile();
