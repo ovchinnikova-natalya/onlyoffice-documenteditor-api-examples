1a0ce7ad03a4182804a0cf86265091a3f88103bb
ovchinnikova-natalya <71496518+ovchinnikova-natalya@users.noreply.github.com>
2020-09-25 16:43:18 +0300

    Delete InsertContent.js

--- InsertContent.js
+++
@@ -1,8 +0,0 @@
-builder.CreateFile("docx");
-var Document = Api.GetDocument();
-var Paragraph = Document.GetElement(0);
-Paragraph = Api.CreateParagraph();
-Paragraph.AddText("This is a sample text. It was inserted here.");
-Document.InsertContent([Paragraph]);
-builder.SaveFile("docx", "InsertContent.docx");
-builder.CloseFile();
