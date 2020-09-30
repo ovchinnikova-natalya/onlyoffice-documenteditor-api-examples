ca37cc388b4566347d055ae87cc46cfc439a8340
ovchinnikova-natalya <71496518+ovchinnikova-natalya@users.noreply.github.com>
2020-09-30 13:05:20 +0300

    Update SetOffset.js

--- spreadsheet/ApiRange/SetOffset.js
+++ spreadsheet/ApiRange/SetOffset.js
@@ -2,6 +2,6 @@ builder.CreateFile("xlsx");
 var Worksheet = Api.GetActiveSheet();
 Worksheet.GetRange("B3").SetValue("This is a sample text with cell offset specified.");
 var Range = Worksheet.GetRange("B3");
-Range.SetOffset(4 * 36000, 5 * 36000);
+Range.SetOffset(2, 1);
 builder.SaveFile("xlsx", "SetOffset.xlsx");
 builder.CloseFile();
