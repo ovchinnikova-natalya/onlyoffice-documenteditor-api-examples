97e8f0dc5759d1e5c00127003499d617f3fa35a1
Natalia <natalia1806@yandex-team.ru>
2020-10-06 10:06:32 +0300

    fix method CreateBlockLvlSdt

---
+++ documenteditor/Api/CreateBlockLvlSdt.js
@@ -0,0 +1,9 @@
+builder.CreateFile("docx");
+var oDocument = Api.GetDocument();
+var oBlockLvlSdt = Api.CreateBlockLvlSdt();
+var oParagraph = Api.CreateParagraph();
+oParagraph.AddText("This is a block text content control.");
+oBlockLvlSdt.AddElement(oParagraph, 0);
+oDocument.AddElement(0, oBlockLvlSdt);
+builder.SaveFile("docx", "CreateBlockLvlSdt.docx");
+builder.CloseFile();
